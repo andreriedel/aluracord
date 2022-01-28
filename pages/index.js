f42c0043eb0ceb64a64e049d9fc2a4fc6a6f15a1
André Riedel <andreriedelz@gmail.com>
2022-01-28 13:39:42 -0300

    aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,7 @@
-import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import { Box, Button, Text, TextField, Image } from "@skynexui/components";
+import React from "react";
+import { useRouter } from "next/router";
 import appConfig from "../config.json";
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-
-      #__next {
-        flex: 1;
-      }
-
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  )
-}
 
 function Title(props) {
   const Tag = props.tag || 'h1';
@@ -51,13 +20,21 @@ function Title(props) {
   );
 }
 
-export default function PaginaInicial() {
-  const username = 'andreriedel';
+export default function IndexPage() {
+  const [userName, setUserName] = React.useState("andreriedel");
+  const [userFollowers, setUserFollowers] = React.useState("");
+  const router = useRouter();
+  
+  fetch(`https://api.github.com/users/${userName}`)
+  .then(
+    response => response.json()
+  )
+  .then(
+    response => {setUserFollowers(response.followers)}
+  );
 
   return (
     <>
-      <GlobalStyle />
-
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -85,6 +62,10 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={(event) => {
+              event.preventDefault();
+              router.push("/chat");
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -93,10 +74,20 @@ export default function PaginaInicial() {
             <Title tag="h2">Boas vindas de volta!</Title>
 
             <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
-              {appConfig.name}
+              Entre no aluracord
             </Text>
 
             <TextField
+              value={userName}
+              onChange={event => {
+                const valor = event.target.value;
+
+                /*
+                Trocar o valor da variavel através do React para que ele lide
+                com a atualização da página.
+                */
+                setUserName(valor);
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -143,7 +134,7 @@ export default function PaginaInicial() {
                 borderRadius: '50%',
                 marginBottom: '16px',
               }}
-              src={`https://github.com/${username}.png`}
+              src={`https://github.com/${userName}.png`}
             />
             
             <Text
@@ -155,7 +146,20 @@ export default function PaginaInicial() {
                 borderRadius: '1000px'
               }}
             >
-              {username}
+              {userName}
+            </Text>
+
+            <Text
+              variant="body4"
+              styleSheet={{
+                color: appConfig.theme.colors.neutrals[200],
+                backgroundColor: appConfig.theme.colors.primary[900],
+                padding: '3px 10px',
+                marginTop: '1em',
+                borderRadius: '1000px'
+              }}
+            >
+              {userFollowers} followers
             </Text>
           </Box>
           {/* Photo Area */}
